0562227ca6f66444ee5cb018ddf8c41821b43fe0
qiqiapple <qiqiapple@gmail.com>
2021-11-29 15:24:41 +0100

    Camera rotation finished
    Finished snap rotation camera.
    Finished mouse rotation camera.

--- Assets/Engineers/Felix/FD_Scripts/FD_CameraFollow.cs
+++ Assets/Engineers/Felix/FD_Scripts/FD_CameraFollow.cs
@@ -1,64 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class FD_CameraFollow : MonoBehaviour
 {
-
     [SerializeField] Transform target;
-    [SerializeField] Vector3 offset;
     [SerializeField] float smoothTime = 0.3f;
     [SerializeField] float rotationSpeedMultiplier = 5f;
-    [SerializeField] float snapRotationSpeedMultiplier = 15f;
+    [SerializeField] float snapRotationSpeedMultiplier = 30f;
     
+    Vector3 offset;
     Vector3 rotationAxis = new Vector3(0, 1, 0);
-    const float rightScreenEdge = 0.9f;
-    const float leftScreenEdge = 0.1f;
+    Vector3 snapRotationAxis = new Vector3(0, 1, 0);
+    const float rightScreenEdge = 0.95f;
+    const float leftScreenEdge = 0.05f;
     float speed;
     
     Vector3 _velocity = Vector3.zero;
 
-
-
-    // Start is called before the first frame update
     void Start(){
         target = FindObjectOfType<FD_PlayerMovement>().transform;
         offset = transform.position - target.position;
     }
 
-    // Update is called once per frame
     void LateUpdate(){
-        RotateCamera();
         Vector3 targetPosition = target.position + offset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, smoothTime);
+        SnapCameraRotation();
+        MouseCameraRotation();
     }
 
-    void RotateCamera(){
+    void SnapCameraRotation(){
+        snapRotationAxis.y = 1;
+        var snapRotation = snapRotationAxis * snapRotationSpeedMultiplier;
         
-        var snapRotation = rotationAxis * snapRotationSpeedMultiplier;
-        rotationAxis.y = 1;
         if (Input.GetKeyDown(KeyCode.E)){
-            transform.Rotate(snapRotation);
-        }
-        if (Input.GetKeyDown(KeyCode.Q)){
             transform.Rotate(-snapRotation);
         }
-        
+        if (Input.GetKeyDown(KeyCode.Q)){
+            transform.Rotate(snapRotation);
+        }
+    }
+
+    void MouseCameraRotation(){
         var mouseRotation = rotationAxis * rotationSpeedMultiplier;
         var currentMousePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         if (currentMousePosition.x > rightScreenEdge){
-            rotationAxis.y = currentMousePosition.x - rightScreenEdge;
+            rotationAxis.y = rightScreenEdge - currentMousePosition.x;
             transform.Rotate(mouseRotation);
         }
         if (currentMousePosition.x < leftScreenEdge){
-            rotationAxis.y = currentMousePosition.x - leftScreenEdge;
+            rotationAxis.y = leftScreenEdge - currentMousePosition.x;
             transform.Rotate(mouseRotation);
         }
-        Debug.Log(currentMousePosition);
     }
-    
-    
-    
+
 }
