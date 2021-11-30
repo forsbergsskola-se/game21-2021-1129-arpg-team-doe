445e6f2442a9af0d48562d6b89ac94727fb43316
Honestflunky8 <90176781+Honestflunky8@users.noreply.github.com>
2021-11-30 10:18:25 +0100

    Moved finished scripts to main game folder

---
+++ Assets/Main Game/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    NavMeshAgent _navMeshAgent;
+
+    void Start(){
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update(){
+        if (Input.GetMouseButton(0)){
+            Mover();
+        }
+    }
+
+    void Mover(){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        _navMeshAgent.destination = hit.point;
+    }
+}
