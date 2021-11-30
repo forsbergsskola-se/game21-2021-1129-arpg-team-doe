9b9af576c980eb7c6e0ff75914a2df0d51153fb5
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2021-11-30 15:06:04 +0100

    Moved unlock gate scripts to main script folder

---
+++ Assets/Engineers/Jesper/JD_Scripts/JD_CursorOnDoor.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class JD_CursorOnDoor : MonoBehaviour
+{
+    internal bool unOpenable = true;
+
+    public Texture2D lockedTexture;
+    public Texture2D unLockedTexture;
+    public Texture2D cursor;
+
+    void Start(){
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    void OnMouseEnter(){
+        if (!unOpenable){
+            Cursor.SetCursor(unLockedTexture, new Vector2(), CursorMode.Auto);
+        }
+        if (unOpenable){
+            Cursor.SetCursor(lockedTexture, new Vector2(), CursorMode.Auto);
+        }
+    }
+
+    void OnMouseExit(){
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
