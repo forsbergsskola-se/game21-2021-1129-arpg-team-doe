a446f4a16dd1ec81a429cbff1e2cfcf4222d1a92
JesDanFors <90110706+JesDanFors@users.noreply.github.com>
2021-12-03 19:01:15 +0100

    Test of walking near not on

---
+++ Assets/Game Designers/Jocke.B/Testable scripts/MoveToObject.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+//===================
+//WIP not yet done
+public class MoveToObject : MonoBehaviour{
+    Movement _movement;
+    GameObject _object;
+
+    bool isObstacle;
+
+    void Start(){
+        _movement = FindObjectOfType<Movement>();
+        _object = gameObject;
+        isObstacle = GetComponent<NavMeshObstacle>() != null;
+    }
+
+    void Update(){
+      //  _movement.Mover(new Vector3(-3.67f, 0.72f, 2.15f));
+    }
+
+    void OnMouseDown(){
+        if (isObstacle){
+            Debug.Log("walking here!");
+            Debug.Log(_object.transform.position - new Vector3(0,_movement.transform.position.y,-2));
+            _movement.Mover(_object.transform.position - new Vector3(0,0,-2));
+        }
+        else if (!isObstacle){
+            Debug.Log("stopping");
+            _movement.StopMoving();
+        }
+    }
+}
