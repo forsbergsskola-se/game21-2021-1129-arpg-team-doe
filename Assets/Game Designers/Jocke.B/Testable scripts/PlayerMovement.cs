482cf084ec1db5d144bd8a58325f496a9e4a237a
Honestflunky8 <90176781+Honestflunky8@users.noreply.github.com>
2021-12-04 11:37:42 +0100

    PlayerMovement added ground check for not walking into objects

--- Assets/Game Designers/Jocke.B/Testable scripts/PlayerMovement.cs
+++ Assets/Game Designers/Jocke.B/Testable scripts/PlayerMovement.cs
@@ -5,20 +5,40 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Movement _navmeshMover;
+    Statistics _statistics;
+    RaycastHit hit;
+    float _interactionRange;
+ 
+   Vector3 _distanceToTarget;
     void Start(){
         _navmeshMover = GetComponent<Movement>();
+        _statistics = GetComponent<Statistics>();
+        _interactionRange = _statistics.InteractRange;
     }
 
     void Update(){
         MoveToCursor();
+        _distanceToTarget = hit.point - transform.position ;
+        
+        // if (_interactionRange > _distanceToTarget){
+        //     _navmeshMover.StopMoving();
+        // }
     }
 
     void MoveToCursor(){
         if (Input.GetMouseButton(0)){
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            
             bool hasHit = Physics.Raycast(ray, out hit);
-            if (hasHit){ _navmeshMover.Mover(hit.point);
+            if (hasHit){
+                if (hit.transform == CompareTag("Ground")){
+                   
+                   _navmeshMover.Mover(hit.point);
+                }
+                else{
+                     _navmeshMover.Mover(hit.point - _distanceToTarget.normalized * 1);
+                }
+                Debug.Log(hit.transform.tag);
             }
         }
     }
