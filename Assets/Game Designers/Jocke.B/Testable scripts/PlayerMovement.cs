b05f6aa6a6ebcc6580eff2ceff09684707b2a8b7
Honestflunky8 <90176781+Honestflunky8@users.noreply.github.com>
2021-12-05 19:55:49 +0100

    Trying to implement Movement sounds

--- Assets/Game Designers/Jocke.B/Testable scripts/PlayerMovement.cs
+++ Assets/Game Designers/Jocke.B/Testable scripts/PlayerMovement.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using FMOD;
+using FMODUnity;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] FMODUnity.EventReference invalidSound;
+    [SerializeField] FMODUnity.EventReference validSound;
+
+    FMOD.Studio.EventInstance _moveInstance;
+    
+    
     Movement _navmeshMover;
     Statistics _statistics;
     RaycastHit hit;
     float _interactionRange;
+    
  
    Vector3 _distanceToTarget;
     void Start(){
         _navmeshMover = GetComponent<Movement>();
         _statistics = GetComponent<Statistics>();
         _interactionRange = _statistics.InteractRange;
+
+        _moveInstance = FMODUnity.RuntimeManager.CreateInstance("event:/Move");
+        
+        _moveInstance.setVolume(50f);
+
+
     }
 
     void Update(){
@@ -32,11 +48,14 @@ public class PlayerMovement : MonoBehaviour
             bool hasHit = Physics.Raycast(ray, out hit);
             if (hasHit){
                 if (hit.transform == CompareTag("Ground")){
-                   
-                   _navmeshMover.Mover(hit.point);
+                    _moveInstance.setParameterByName("MoveFeedback", 0f);
+                    
+                    //_moveInstance.release();
+                    _navmeshMover.Mover(hit.point);
                 }
                 else{
                      _navmeshMover.Mover(hit.point - _distanceToTarget.normalized * 1);
+                     _moveInstance.setParameterByName("MoveFeedback", 1f);
                 }
                 Debug.Log(hit.transform.tag);
             }
