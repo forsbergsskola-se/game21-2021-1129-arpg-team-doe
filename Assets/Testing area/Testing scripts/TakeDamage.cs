7436f994bc7491b517b5974d31f752ad2f1b78d2
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2021-12-07 11:16:42 +0100

    Added TakeDamage Logic

---
+++ Assets/Testing area/Testing scripts/TakeDamage.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class TakeDamage : MonoBehaviour{
+    
+    Statistics _stats;
+    Random random;
+
+    int _currentHealth;
+    bool _dodged;
+
+    void Start(){
+        _stats = GetComponent<Statistics>();
+        _currentHealth = _stats.currentHP;
+        random = new Random();
+    }
+
+    public void ReceiveDamage(int damage){ //Toughness should affect this
+        _currentHealth -= DamageCalc(damage);
+    }
+    
+    bool DodgeDamage(){
+        var dodgeChance = _stats.Reflex;
+        
+        if (random.NextDouble() < dodgeChance){ 
+            _dodged = true;
+        }
+        else{
+            _dodged = false;
+        }
+        return _dodged;
+    }
+
+    int DamageCalc(int dmg){
+        if (DodgeDamage()){
+            dmg = 0;
+        }
+        return dmg;
+    }
+}
