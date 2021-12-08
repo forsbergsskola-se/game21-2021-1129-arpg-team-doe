3d64e69be6544f4b0ea6835f0930642fd4715795
Honestflunky8 <90176781+Honestflunky8@users.noreply.github.com>
2021-12-08 21:14:45 +0100

    Added Item creation tools in inspector

---
+++ Assets/Testing area/Testing scripts/Scriptable Objects/Items/Scripts/ConsumableObject.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Consumable Object", menuName = "Inventory System/Items/Consumable")]
+public class ConsumableObject : ItemObject{
+   public int restoreHealthValue;
+   [Min(0f)][Tooltip	("Duration in Seconds")] public int buffDuration;
+   public int toughnessBuff;
+   public int strengthBuff;
+   public int dexterityBuff;
+   public int knowledgeBuff;
+   public int luckBuff;
+   [Tooltip	("Amount of attacks per second Buff")] public int attackSpeedBuff;
+   public int damageBuff;
+   public AudioClip consumeSound;
+
+   public void Awake(){
+      type = ItemType.Consumable;
+   }
+}
