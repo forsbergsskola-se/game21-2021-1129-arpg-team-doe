d00b6811738411463c591ab8d22ab36e87cfa21e
Felix K <felix.kjellberg@student.forsbergsskola.se>
2021-12-08 11:55:11 +0100

    Fixed testing scene

--- Assets/Testing area/Testing scripts/DealDamage.cs
+++ Assets/Testing area/Testing scripts/DealDamage.cs
@@ -2,17 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public interface IDamageReceiver{
     void ReceiveDamage(int damage);
 }
 
 public class DealDamage : MonoBehaviour{
+    [SerializeField] float critDamageMultiplier = 1.5f;
 
     Statistics _statistics;
+    Random _random;
 
     float _attackRange;
     float _attackSpeed;
+    float _critChance;
     float _distance;
     bool isRanged;
 
@@ -20,28 +24,27 @@ public class DealDamage : MonoBehaviour{
         _statistics = GetComponent<Statistics>();
         _attackRange = _statistics.AttackRange;
         _attackSpeed = _statistics.AttackSpeed;
+        _random = new Random();
         InvokeRepeating(nameof(Attack),0, 1f/ _attackSpeed);
     }
 
     public int Attack(int damage, GameObject target){
-        if (isRanged){
-            damage = DoRangedDamage(damage, target);
+        if (_random.NextDouble() < _critChance){
+            damage = Mathf.RoundToInt(damage * critDamageMultiplier);
         }
-        else{
-            damage = DoMeleeDamage(damage, target);
-        }
+
         target.GetComponent<IDamageReceiver>()?.ReceiveDamage(damage); // check!!!
         Debug.Log("Dealing " + damage + " Damage");
         return damage;
     }
 
-    int DoMeleeDamage(int damage, GameObject target){
-        return DamageManipulation(damage, target, _statistics.Strength);
-    }
-
-    int DoRangedDamage(int damage, GameObject target){
-        return DamageManipulation(damage, target, _statistics.Dexterity);
-    }
+    // int DoMeleeDamage(int damage, GameObject target){
+    //     return DamageManipulation(damage, target, _statistics.Strength);
+    // }
+    //
+    // int DoRangedDamage(int damage, GameObject target){
+    //     return DamageManipulation(damage, target, _statistics.Dexterity);
+    // }
 
     int DamageManipulation(int damage, GameObject target, float modifier ){
         _distance = Vector3.Distance(transform.position, target.transform.position);
