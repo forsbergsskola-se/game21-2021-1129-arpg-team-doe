4b4e1954763c2f1bbbfca3cd3b3e0acb3673bc65
Felix K <felix.kjellberg@student.forsbergsskola.se>
2021-12-09 16:41:58 +0100

    Updated enemy die to be called only once

--- Assets/Testing area/Testing scripts/Enemy.cs
+++ Assets/Testing area/Testing scripts/Enemy.cs
@@ -4,19 +4,33 @@ public class Enemy : MonoBehaviour{
 
    Statistics _statistics;
    CapsuleCollider _capsuleCollider;
+   Fighter _fighter;
+   EnemyMovement _enemyMovement;
+   bool isDead;
 
    void Start(){
       _statistics = GetComponent<Statistics>();
       _capsuleCollider = GetComponent<CapsuleCollider>();
+      _fighter = GetComponent<Fighter>();
+      _enemyMovement = GetComponent<EnemyMovement>();
    }
 
    void Update(){
-      if (!_statistics.IsAlive) { Die(); } // debug
+      if (IsDead()) { Die(); } // debug
    }
 
    void Die(){
       // call death event (animations, sounds etc)
       Debug.Log(this.name + " Dead");
       _capsuleCollider.enabled = false;
+      _fighter.enabled = false;
+      _enemyMovement.enabled = false;
+      isDead = true;
+   }
+
+   bool IsDead(){
+      if (!_statistics.IsAlive && !isDead){ return true;}
+
+      return false;
    }
 }
