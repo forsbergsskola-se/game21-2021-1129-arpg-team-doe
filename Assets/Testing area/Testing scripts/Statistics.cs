b92a1d1f36946b192a91e4a72351ae67d9c008c8
Efraim Kent <efraim.kent@student.forsbergsskola.se>
2021-12-10 11:07:54 +0100

    Added DodgeChance and DamageMultiplier/CalculateWeaponDamage

--- Assets/Testing area/Testing scripts/Statistics.cs
+++ Assets/Testing area/Testing scripts/Statistics.cs
@@ -1,32 +1,45 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Statistics : MonoBehaviour{
 
-    [SerializeField] float toughness, strength, dexterity, knowledge, reflex, luck, interactRange, attackRange, attackSpeed;
+    int weaponDamage = 1; // for debug
+    bool isRanged;
+    int damage;
+
+    [SerializeField]
+    float toughness, strength, dexterity, knowledge, reflex, luck, interactRange, attackRange, attackSpeed;
+
+    float dodgeChance;
 
     public float Toughness{
         get => toughness;
         private set => toughness = value;
     }
+
     public float Strength{
         get => strength;
         private set => strength = value;
     }
+
     public float Dexterity{
         get => dexterity;
         private set => dexterity = value;
     }
+
     public float Knowledge{
         get => knowledge;
         private set => knowledge = value;
     }
-    public float Reflex{ //0-100 dodge chance
+
+    public float DodgeChance{
+        get => dodgeChance;
+    }
+    public float Reflex{
+        //0-100 dodge chance
         get => reflex;
         private set => reflex = value;
     }
+
     public float Luck{
         get => luck;
         private set => luck = value;
@@ -35,24 +48,29 @@ public class Statistics : MonoBehaviour{
     public int maxHP;
     public int currentHP { get; private set; }
 
-    public float InteractRange{
-        get{ return interactRange; }
-        private set{ interactRange = value; }
+    public float InteractRange
+    {
+        get { return interactRange; }
+        private set { interactRange = value; }
     }
-    public float AttackRange{
-        get{ return attackRange; }
-        private set{ attackRange = value; }
+
+    public float AttackRange
+    {
+        get { return attackRange; }
+        private set { attackRange = value; }
     }
-    public float AttackSpeed{
-        get{ return attackSpeed; }
-        private set{ attackSpeed = value * (this.dexterity * 0.01f + 1); }
+
+    public float AttackSpeed
+    {
+        get { return attackSpeed; }
+        private set { attackSpeed = value * (this.dexterity * 0.01f + 1); }
     }
 
     public bool IsAlive => currentHP > 0;
+    public bool dealingDmg; //used for debug
 
-        public bool dealingDmg; //used for debug
-
-    public Statistics(float toughness, float strength, float dexterity, float knowledge, float reflex, float luck, float interactRange, float attackRange, float attackSpeed, int maxHp, bool dealingDmg){
+    public Statistics(float toughness, float strength, float dexterity, float knowledge, float reflex, float luck,
+        float interactRange, float attackRange, float attackSpeed, int maxHp, bool dealingDmg) {
         this.toughness = toughness;
         this.strength = strength;
         this.dexterity = dexterity;
@@ -70,10 +88,6 @@ public class Statistics : MonoBehaviour{
         currentHP = maxHP;
     }
 
-    void Update(){
-        DealDmg();
-    }
-
     // maybe change later
     public void UpdateHealth(int healthChange){
         currentHP -= healthChange;
@@ -84,12 +98,26 @@ public class Statistics : MonoBehaviour{
         return attackSpeed * (this.dexterity * 0.01f + 1);
     }
 
-    public void DealDmg(){ //used for debug
-        if (Input.GetKeyDown(KeyCode.E)){
-            dealingDmg = true;
-            currentHP -= 10;
-            FindObjectOfType<UIDamageNr>().Display();
+    float CalculateDodgeChance(){
+        return dodgeChance = this.reflex * 0.01f;
+    }
 
+    int CalculateWeaponDamage(int weaponDamage){
+        float damageMultiplier = 0f;
+        if (isRanged){
+            damageMultiplier = (this.dexterity * 0.01f + 1);
         }
+        if (!isRanged){
+            damageMultiplier = (this.strength * 0.01f + 1);
+        }
+        return damage = (int) (weaponDamage * damageMultiplier);
+    }
+
+    float DamageMultiplier(){
+        float damageMultiplier;
+        if (isRanged){
+            return damageMultiplier = (this.dexterity * 0.01f + 1);
+        }
+        return damageMultiplier = (this.strength * 0.01f + 1);
     }
 }
