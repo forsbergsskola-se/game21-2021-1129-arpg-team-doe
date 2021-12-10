a43fb5799cdb1500d8a1b26d5fa3f226c2370f12
qiqiapple <qiqiapple@gmail.com>
2021-12-10 13:59:23 +0100

    Update in Statistics

--- Assets/Testing area/Testing scripts/TakeDamage.cs
+++ Assets/Testing area/Testing scripts/TakeDamage.cs
@@ -41,9 +41,7 @@ public class TakeDamage : MonoBehaviour, IDamageReceiver{
     }
 
     bool DodgeDamage(){
-        var dodgeChance = _stats.Reflex;//This calculation should be in statistics?
-
-        if (random.NextDouble() < dodgeChance){
+        if (random.NextDouble() < _stats.DodgeChance){
             _dodged = true;
         }
         else{
