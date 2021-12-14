051a2d3bf4e843039e9e0a7e28b39c15a7504ae8
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2021-12-14 15:33:43 +0100

    Finished DisplayInventory script

--- Assets/Testing area/Testing scripts/DisplayInventory.cs
+++ Assets/Testing area/Testing scripts/DisplayInventory.cs
@@ -21,14 +21,30 @@ public class DisplayInventory : MonoBehaviour{
 
     // Update is called once per frame
     void Update(){
-        //UpdateDisplay();
+        UpdateDisplay();
     }
 
+    public void UpdateDisplay(){
+        for (int i = 0; i < inventory.Container.Count; i++){
+            if (itemsDisplayed.ContainsKey(inventory.Container[i])){
+                itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text =
+                    inventory.Container[i].amount.ToString("n0");
+            }
+            else{
+                var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+                itemsDisplayed.Add(inventory.Container[i], obj);
+            }
+        }
+    }
+    
     public void CreateDisplay(){
         for (int i = 0; i < inventory.Container.Count; i++){
             var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
             obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            itemsDisplayed.Add(inventory.Container[i], obj);
         }
     }
 
