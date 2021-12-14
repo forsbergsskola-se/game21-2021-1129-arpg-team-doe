9caf7a626ed9e12a59c40661202d4d61190bc448
Efraim Kent <efraim.kent@student.forsbergsskola.se>
2021-12-14 18:04:18 +0100

    Added XP Drop Event and Eventlistener script + objects

---
+++ Assets/Testing area/Testing scripts/Scriptable Objects/Events/Scripts/XPDropEventListener.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XPDropEventListener : MonoBehaviour
+{
+    [SerializeField] XPDropEvent _xpDropEvent;
+
+    void Awake() => _xpDropEvent.Register(this);
+    void OnDestroy() => _xpDropEvent.Deregister(this);
+    public void RaiseEvent(int xpAmount) => gameObject.GetComponent<Level>().LevelingGameObject.ReceiveXP(xpAmount);
+}
