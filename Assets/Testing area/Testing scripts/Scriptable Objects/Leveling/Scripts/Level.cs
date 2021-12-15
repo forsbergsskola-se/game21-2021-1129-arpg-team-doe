e660e9f9f9ebd60516650a084b78b69012cb4202
Honestflunky8 <90176781+Honestflunky8@users.noreply.github.com>
2021-12-15 11:51:02 +0100

    Implemented sound on level up

--- Assets/Testing area/Testing scripts/Scriptable Objects/Leveling/Scripts/Level.cs
+++ Assets/Testing area/Testing scripts/Scriptable Objects/Leveling/Scripts/Level.cs
@@ -1,9 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using FMODUnity;
 using UnityEngine;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 public class Level : MonoBehaviour
 {
     [SerializeField] public LevelingGameObject LevelingGameObject;
+    [SerializeField] public FMODUnity.EventReference fmodEvent;
+    
+    FMOD.Studio.EventInstance instance;
+    void Start(){
+        if (!fmodEvent.IsNull){
+            instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        }
+    }
+    public void PlaySound(){
+        instance.start();
+        instance.release();
+    }
 }
