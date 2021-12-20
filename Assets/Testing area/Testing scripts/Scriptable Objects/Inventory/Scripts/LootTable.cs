f509da9dbf34299b9c9c6fabb4a2bcd54924b962
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2021-12-20 13:17:41 +0100

    Adding loottable

---
+++ Assets/Testing area/Testing scripts/Scriptable Objects/Inventory/Scripts/LootTable.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class LootTable : MonoBehaviour{
+    public int[] table ={60, 30, 10};
+}
