cf94fcbf99c9b1febe95e51f7c58e38cdd10c543
JesDanFors <90110706+JesDanFors@users.noreply.github.com>
2021-12-20 18:44:15 +0100

    Update to audiolistener

--- Assets/Testing area/Testing scripts/AudioFollower.cs
+++ Assets/Testing area/Testing scripts/AudioFollower.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class AudioFollower : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class AudioFollower : MonoBehaviour{
+    [SerializeField] Vector3 offset;
+    
+    Transform player;
+    Transform camera;
+
+    void Start(){
+        player = GetComponentInParent<PlayerController>().transform;
+        camera = GetComponentInParent<CameraFollow>().transform;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        var transform1 = transform;
+        transform1.position = player.position + offset;
+        var transform1Rotation = transform1.rotation;
+        transform1Rotation.eulerAngles = camera.rotation.eulerAngles;
+        transform.position = transform1.position;
+        transform.rotation = transform1.rotation;
     }
 }
