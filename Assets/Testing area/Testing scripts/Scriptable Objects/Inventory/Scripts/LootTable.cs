d454d60b26b0a42afc47a0652134c9c2aa5f7c40
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2021-12-21 11:28:12 +0100

    Added loot table, version 1 probability test

--- Assets/Testing area/Testing scripts/Scriptable Objects/Inventory/Scripts/LootTable.cs
+++ Assets/Testing area/Testing scripts/Scriptable Objects/Inventory/Scripts/LootTable.cs
@@ -4,6 +4,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = System.Random;
 
-public class LootTable : MonoBehaviour{
-    public int[] table ={60, 30, 10};
+[CreateAssetMenu]
+public class LootTable : ScriptableObject{
+    
+    [Serializable]
+    public class Drop{
+        public ItemObject drop;
+        public int probability;
+    }
+
+    public List<Drop> table;
+
+    public ItemObject GetDrop(){
+        int roll = UnityEngine.Random.Range(0, 100);
+
+        for (int i = 0; i < table.Count; i++){
+            roll -= table[i].probability;
+            if (roll < 0){
+                return table[i].drop;
+            }
+        }
+
+        return table[0].drop;
+    }
 }
