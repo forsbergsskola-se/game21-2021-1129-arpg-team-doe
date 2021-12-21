2c2e79b93eaeab1b3e11e1c5e896e8a96f9ec804
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2021-12-21 11:41:46 +0100

    Replaced probability with weightened lootTable

--- Assets/Testing area/Testing scripts/Scriptable Objects/Inventory/Scripts/LootTable.cs
+++ Assets/Testing area/Testing scripts/Scriptable Objects/Inventory/Scripts/LootTable.cs
@@ -10,16 +10,37 @@ public class LootTable : ScriptableObject{
     [Serializable]
     public class Drop{
         public ItemObject drop;
-        public int probability;
+        public int weight;
     }
 
     public List<Drop> table;
+    
+    [NonSerialized]
+    int totalWeight = -1;
+
+    public int TotalWeight{
+        get{
+            if (totalWeight == -1){
+                CalculateTotalWeight();
+            }
+
+            return totalWeight;
+        }
+    }
+
+    void CalculateTotalWeight(){
+        totalWeight = 0;
+        for (int i = 0; i < table.Count; i++){
+            totalWeight += table[i].weight;
+        }
+    }
 
     public ItemObject GetDrop(){
-        int roll = UnityEngine.Random.Range(0, 100);
+        int roll = UnityEngine.Random.Range(0, TotalWeight);
 
         for (int i = 0; i < table.Count; i++){
-            roll -= table[i].probability;
+            roll -= table[i].weight;
+            
             if (roll < 0){
                 return table[i].drop;
             }
