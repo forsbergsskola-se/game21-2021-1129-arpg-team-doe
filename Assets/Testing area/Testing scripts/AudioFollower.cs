e3f17be66c4718e7c339935b166eb64d32929cb8
JesDanFors <90110706+JesDanFors@users.noreply.github.com>
2021-12-21 10:49:26 +0100

    Listener now follows camera rotation

--- Assets/Testing area/Testing scripts/AudioFollower.cs
+++ Assets/Testing area/Testing scripts/AudioFollower.cs
@@ -5,20 +5,15 @@ public class AudioFollower : MonoBehaviour{
     [SerializeField] Vector3 offset;
     
     Transform player;
-    Transform camera;
 
     void Start(){
-        player = GetComponentInParent<PlayerController>().transform;
-        camera = FindObjectOfType<CameraFollow>().transform;
+        player = FindObjectOfType<PlayerController>().transform;
     }
 
     void Update(){
         var transform1 = transform;
         transform1.position = player.position + offset;
-        var transform1Rotation = transform1.rotation;
-        transform1Rotation.eulerAngles = camera.rotation.eulerAngles;
         transform.position = transform1.position;
-        transform.rotation = transform1.rotation;
     }
 
 }
