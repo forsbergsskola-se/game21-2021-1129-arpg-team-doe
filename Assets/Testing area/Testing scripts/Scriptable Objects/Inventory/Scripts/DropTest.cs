40c60f849da7e6445ab38ef7ceee422a2997ad7d
Pavel Sarmiento <pavel.sarmiento.kochersky@student.forsbergsskola.se>
2022-01-03 11:16:34 +0100

    Drop items from loot Table done?

--- Assets/Testing area/Testing scripts/Scriptable Objects/Inventory/Scripts/DropTest.cs
+++ Assets/Testing area/Testing scripts/Scriptable Objects/Inventory/Scripts/DropTest.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class DropTest : MonoBehaviour{
     [SerializeField] LootTable lootTable;
+    [SerializeField] GameObject cash;
+    [SerializeField] float spreadRange;
     
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space)){
-            ItemObject itemObject = lootTable.GetDrop();
+        if (Input.GetKeyDown(KeyCode.Space)){
+            GameObject itemObject = lootTable.GetDrop();
+            Instantiate(itemObject, transform.position + RandomLocation(), Quaternion.identity);
+            Instantiate(cash, transform.position + RandomLocation(), Quaternion.identity);
             Debug.Log(itemObject.name);
         }
+    }
+    
+    Vector3 RandomLocation(){
+        //may change x and z between 1-3?
+        var position = new Vector3((Random.insideUnitSphere.x * spreadRange), 0f,
+            (Random.insideUnitSphere.z * spreadRange));
+        return position;
     }
 }
