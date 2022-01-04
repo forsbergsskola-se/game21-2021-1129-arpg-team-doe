51283bda1baaa40712d2ca66f2a6ddb892be5964
Felix K <felix.kjellberg@student.forsbergsskola.se>
2022-01-04 09:32:36 +0100

    Some cleanup

--- Assets/Testing area/Testing scripts/UI/UIDamageNr.cs
+++ Assets/Testing area/Testing scripts/UI/UIDamageNr.cs
@@ -10,18 +10,14 @@ public class UIDamageNr : MonoBehaviour, IDamageNumbers, IHealthListener{
 
     [SerializeField] float duration;
     [SerializeField] TextMeshProUGUI _textMeshProUGUI;
-
-    Animator _animator;
+    
 
     string dmgText;
     bool activeTimer;
     bool takingDamage;
 
     const string FLOATING_POINT = "FloatingPoint";
-
-    void Start(){
-        _animator = gameObject.GetComponent<Animator>();
-    }
+    
 
     public void DisplayDmg(int damage, bool isCrit){
         dmgText = Convert.ToString(damage);
