48030b53c7426739b6d988f91542415091b817da
Felix K <felix.kjellberg@student.forsbergsskola.se>
2022-01-05 10:55:05 +0100

    some cleanup

--- Assets/Testing area/Testing scripts/UI/HealthBar.cs
+++ Assets/Testing area/Testing scripts/UI/HealthBar.cs
@@ -8,8 +8,7 @@ public interface IHealthbar{
     void SetSliderCurrentHealth(int currentHealth);
 }
 public class HealthBar : MonoBehaviour, IHealthbar, IHealthListener{
-
-    //public Slider slider;
+    
     public Image healthBar;
 
     GameObject _parent;
@@ -18,7 +17,6 @@ public class HealthBar : MonoBehaviour, IHealthbar, IHealthListener{
 
     public void Start() {
         _health = GetComponentInParent<Health>();
-        // SetSliderMaxHealth(_health.ModifiedMaxHP);
         _parent = GetComponentInParent<ToggleHealthBar>()?.gameObject;
         
     }
@@ -27,12 +25,6 @@ public class HealthBar : MonoBehaviour, IHealthbar, IHealthListener{
         transform.rotation = Quaternion.RotateTowards(transform.rotation, Camera.main.transform.rotation, 1f);
         transform.position = _parent.transform.position + _offset;
     }
-
-    // void SetSliderMaxHealth(float maxHP){
-    //     slider.maxValue = maxHP;
-    //     slider.minValue = maxHP - maxHP;
-    //     slider.value = maxHP;
-    // }
 
     public void SetSliderCurrentHealth(int currentHealth){
         var healthValue = ((float)currentHealth/(float)_health.ModifiedMaxHP);
