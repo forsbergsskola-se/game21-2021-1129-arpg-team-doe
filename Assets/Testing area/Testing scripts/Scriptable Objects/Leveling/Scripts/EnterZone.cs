995add890b1ca993350634ca8d49ab7d27c219e6
Felix K <felix.kjellberg@student.forsbergsskola.se>
2022-01-06 11:26:44 +0100

    leveling scripts clean up

--- Assets/Testing area/Testing scripts/Scriptable Objects/Leveling/Scripts/EnterZone.cs
+++ Assets/Testing area/Testing scripts/Scriptable Objects/Leveling/Scripts/EnterZone.cs
@@ -6,15 +6,12 @@ using UnityEngine;
 public class EnterZone : MonoBehaviour{
     [SerializeField] XPDrop _xpDrop;
     
-
     public void OnTriggerEnter(Collider other){
         if (other.tag == "Player"){
-             if (_xpDrop != null){
-                _xpDrop._xpDropEvent.Invoke(_xpDrop.xpAmount);
-                _xpDrop = null;
-             }
-            
-            
+            if (_xpDrop != null){
+               _xpDrop._xpDropEvent.Invoke(_xpDrop.xpAmount);
+               _xpDrop = null;
+            }
         }
     }
 }
--- Assets/Testing area/Testing scripts/Scriptable Objects/Leveling/Scripts/Level.cs
+++ Assets/Testing area/Testing scripts/Scriptable Objects/Leveling/Scripts/Level.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using FMODUnity;
 using UnityEngine;
-using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 public class Level : MonoBehaviour
 {
     [SerializeField] public LevelingGameObject LevelingGameObject;
     [SerializeField] public FMODUnity.EventReference fmodEvent;
-    //[SerializeField] public GameEventListener getXPEventListener;
 
     FMOD.Studio.EventInstance instance;
     void Start(){
