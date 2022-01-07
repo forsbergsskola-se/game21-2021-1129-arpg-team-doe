37964aa93b248eb2165831faaa6ebc5954130d56
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-01-07 09:58:37 +0100

    Implemented (Not working) Skill Point Display in stat screen

--- Assets/Main Game/Scripts/UI/UIStatScreen.cs
+++ Assets/Main Game/Scripts/UI/UIStatScreen.cs
@@ -1,11 +1,51 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIStatScreen : MonoBehaviour{
     [SerializeField] Button applyButton;
+    [SerializeField] GameObject SkillPointTextObject;
+    [SerializeField] LevelingGameObject _playerLevel;
     
+    int availableSkillPoints;
+
+    string SkillPointText;
+
+    void Awake()
+    {
+        SkillPointText = SkillPointTextObject.GetComponent<TextMeshProUGUI>().text;
+    }
+
+    void Start()
+    {
+        SetAvailableSkillPoints();
+        SetSkillPointText();
+    }
+
+    void Update()
+    {
+        
+        if (availableSkillPoints != _playerLevel.skillPoint)
+        {
+            
+            SetAvailableSkillPoints();
+            SetSkillPointText();
+        }
+    }
+
     [ContextMenu("OnDisable")]
     void OnDisable(){
         applyButton.onClick.Invoke();
+    }
+
+    public void SetAvailableSkillPoints()
+    {
+        availableSkillPoints = _playerLevel.skillPoint;
+    }
+
+    public void SetSkillPointText()
+    {
+        SkillPointText = ("Skill Points: " + availableSkillPoints.ToString()); //Skill points;
     }
 }
