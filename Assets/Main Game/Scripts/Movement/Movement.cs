2a374d3485f1ca48c6df82d508a981c55cfaa8c4
JesDanFors <90110706+JesDanFors@users.noreply.github.com>
2022-01-10 13:35:01 +0100

    Made movementsound 3D

--- Assets/Main Game/Scripts/Movement/Movement.cs
+++ Assets/Main Game/Scripts/Movement/Movement.cs
@@ -22,6 +22,7 @@ public class Movement : MonoBehaviour{
         _animationController = GetComponentInChildren<AnimationController>();
         _fighter = GetComponent<Fighter>();
         _movementInstance = FMODUnity.RuntimeManager.CreateInstance(movementSound);
+        _movementInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject, GetComponent<Rigidbody>()));
     }
 
     
@@ -49,7 +50,7 @@ public class Movement : MonoBehaviour{
     }
 
     void PlayMovementSound(){
-        _movementInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject, GetComponent<Rigidbody>()));
+       // _movementInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject, GetComponent<Rigidbody>()));
         _movementInstance.getPlaybackState(out var playbackState);
             if (playbackState == PLAYBACK_STATE.STOPPED){
                 _movementInstance.start();
