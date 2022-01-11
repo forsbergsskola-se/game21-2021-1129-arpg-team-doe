0aab283af25d0b4b08441d328702cc4666cafa47
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-01-11 19:46:04 +0100

    Fixed Exit

--- Assets/Main Game/Scripts/Controllers/SceneLoader.cs
+++ Assets/Main Game/Scripts/Controllers/SceneLoader.cs
@@ -7,36 +7,57 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using STOP_MODE = FMODUnity.STOP_MODE;
 
-[RequireComponent(typeof(InteractableObject))]
+//[RequireComponent(typeof(InteractableObject))]
 [DisallowMultipleComponent]
 public class SceneLoader : MonoBehaviour,Iinteractable
 {
-FMOD.Studio.Bus bus;
-
-void Start()
-{
-    bus = FMODUnity.RuntimeManager.GetBus("bus:/Main Bus");
-}
-
-public void Use(){
-        Invoke(nameof(LoadScene), 1f);
-    }
-
-    public void LoadScene()
+    [SerializeField] Texture2D defaultCursor;
+    void OnTriggerEnter(Collider collider)
     {
-        // player.SetActive(false);
-        SceneManager.LoadSceneAsync("Scene2");
-        bus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-        StartCoroutine(StopMusic());
+        if (collider.transform.tag == "Player")
+        {
+            Invoke(nameof(LoadScene), 0.3f);
+        }
     }
 
-    IEnumerator StopMusic()
-    {
-        bus.getVolume(out float volume);
-        bus.setVolume(0);
-        yield return new WaitForSeconds(1);
-        bus.setVolume(volume);
+    void LoadScene()
+    { 
+        Cursor.SetCursor(defaultCursor, Vector2.zero , CursorMode.ForceSoftware);
+        SceneManager.LoadScene("Scene2");
     }
+
+
+    // FMOD.Studio.Bus bus;
+//
+// void Start()
+// {
+//     bus = FMODUnity.RuntimeManager.GetBus("bus:/Main Bus");
+// }
+//
+
+
+
+//
+// public void Use(){
+//         Invoke(nameof(LoadScene), 0.3f);
+//     }
+//
+//     public void LoadScene()
+//     {
+//         // player.SetActive(false);
+//         //bus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+//         SceneManager.LoadScene("Scene2");
+//        // StartCoroutine(StopMusic());
+//     }
+//
+//     IEnumerator StopMusic()
+//     {
+//         bus.getVolume(out float volume);
+//         Debug.Log(volume);
+//         bus.setVolume(0);
+//         yield return new WaitForSeconds(0.5f);
+//         bus.setVolume(1);
+//     }
     
     
 }
