96ad03fb9e0893501cd6af0d3cdf65a6af58068e
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-01-11 18:34:01 +0100

    Fixed Enemy Sliding and started working on vanishing lady sounds

--- Assets/Main Game/Scripts/Enemy/VanishingLady.cs
+++ Assets/Main Game/Scripts/Enemy/VanishingLady.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
 
 public class VanishingLady : MonoBehaviour
 {
    AnimationController _animationController;
+   [SerializeField] EventReference soundWhileTurning;
+   [SerializeField] EventReference soundWhenVanishing;
+   
+   EventInstance soundWhileTurningInstance;
+   EventInstance soundWhenVanishingInstance;
 
     void Start()
     {
@@ -22,8 +29,15 @@ public class VanishingLady : MonoBehaviour
 
     IEnumerator TurnThenVanish()
     {
+        
+        // //soundWhileTurningInstance = RuntimeManager.CreateInstance(soundWhileTurning);
+        // soundWhenVanishingInstance = RuntimeManager.CreateInstance(soundWhenVanishing);
+        // soundWhileTurningInstance.start();
+        // soundWhileTurningInstance.release();
         _animationController.ChangeAnimationState("Turn");
         yield return new WaitForSeconds(3);
+        // soundWhenVanishingInstance.start();
+        // soundWhenVanishingInstance.release();
         gameObject.SetActive(false);
     }
 }
